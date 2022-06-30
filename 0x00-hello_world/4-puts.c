ea6f1c0ba21955477b16ff0b6b4f97ba563ca48f
betty <cabdallac808@gmail.com>
2022-06-30 03:21:34 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /*
  * this is the main function
  */
 int main (void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("Programming is like building a multilingual puzzle");
 	return (0);
 }
