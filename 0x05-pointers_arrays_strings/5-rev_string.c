84ba6b2b60360ccb8580fe3b1146d553b9355182
betty <cabdallac808@gmail.com>
2022-07-11 07:21:13 -0700

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * rev_string - reserves string
+ * @s: variable type char
+ *
+ * Return: no return.
+ */
+
+void rev_string(char *s)
+{
+	int count, i, j;
+	char *str;
+
+	count = 0;
+	while (count >= 0)
+	{
+		if (s[count] == '\0')
+		{
+			break;
+		}
+		count++;
+	}
+	str = s;
+	for (i = 0; i <= (count - 1); i++)
+	{
+		for (j = i + 1; j > 0; j--)
+		{
+			temp = *(str + j);
+			*(str + j) = *(str - 1);
+			*(str - 1) = temp;
+		}
+	}
+}
