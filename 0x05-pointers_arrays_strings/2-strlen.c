ca7e73ceb01d0f5f3e59ee9311cf18baaebaf481
betty <cabdallac808@gmail.com>
2022-07-11 06:06:06 -0700

    returns the length of a string.

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strlen -  returns the length of a string.
+ * @s: varaible type char
+ *
+ * Return:v length of string.
+ */
+
+int _strlen(char *s)
+{
+	strlen(s);
+}
