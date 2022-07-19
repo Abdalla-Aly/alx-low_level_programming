f5a74a33bd6c1ff97502ff448e68eb8a20d70715
betty <cabdallac808@gmail.com>
2022-07-18 20:52:03 -0700

    hot fix

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include "main.h"
 
+/**
+ * _memcpy - copies memory area.
+ * @dest: param type char.
+ * @src: param type char.
+ * @n: param type int.
+ *
+ * Return: pointer dest.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	return (dest);
+}
