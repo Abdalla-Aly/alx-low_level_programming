2dce5723136d2225d03fa5e97c2c59ec9096584c
betty <cabdallac808@gmail.com>
2022-07-18 23:23:39 -0700

    hot fix

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -18,16 +18,14 @@ unsigned int _strspn(char *s, char *accept)
 		bool = 1;
 		for (j = 0; *(accept + j) != '\0'; j++)
 		{
-			if (*(s + i) == *(accept +j))
+			if (*(s + i) == *(accept + j))
 			{
 				bool = 0;
 				break;
 			}
 		}
 		if (bool == 1)
-		{
-			 break;
-		}
+			break;
 	}
 	return (i);
 }
