4ea25286511489fc7c8f984b757dc4208c0d39d0
betty <cabdallac808@gmail.com>
2022-07-20 01:59:43 -0700

    hot fix

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,11 +9,11 @@
  */
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	if (*(s + i) > '\0')
+	if (*s == '\0')
 	{
-		printf("%s\n", s);
+		_putchar('\n');
+		return;
 	}
-	printf("\n");
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
