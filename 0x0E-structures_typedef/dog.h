3e9027057439866d58dde8c1341cd176eae681c7
betty <cabdallac808@gmail.com>
2022-08-02 07:08:38 -0700

    struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -23,6 +23,6 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 int _strlen(char *str);
 char *_strcpy(char *dest, char *src);
-char *new_dog(char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
